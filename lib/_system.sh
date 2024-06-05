0d32bb11633373a1b8676cb23e87c384083d27da
OtavioSR <105518647+OtavioSR@users.noreply.github.com>
2024-06-05 17:42:24 -0300

    Revert "Update _system.sh"
    This reverts commit 9c58e4893a277449a11f1b5fe1456cbd0090d6c0.

--- lib/_system.sh
+++ lib/_system.sh
@@ -285,31 +285,17 @@ system_node_install() {
   sudo apt-get install -y nodejs
   sleep 2
   npm install -g npm@latest
-
-  EOF
-
-  }
-
-  system_postgres_install() {
-  print_banner
-  printf "${WHITE} 💻 Instalando Postgres...${GRAY_LIGHT}"
-  printf "\n\n"
-
   sleep 2
-  sudo su - root <<EOF
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sudo apt-get update -y && sudo apt-get -y install postgresql
   sleep 2
   sudo timedatectl set-timezone America/Sao_Paulo
   
-  EOF
+EOF
 
   sleep 2
-
-  }
-  
-
+}
 #######################################
 # installs docker
 # Arguments:
