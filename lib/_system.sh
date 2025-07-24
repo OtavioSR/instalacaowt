c9cc0086bdbcc3406a53f612087ed7986278386b
OtavioSR <105518647+OtavioSR@users.noreply.github.com>
2025-07-24 14:16:12 -0300

    ajustes

--- lib/_system.sh
+++ lib/_system.sh
@@ -320,9 +320,11 @@ system_docker_install() {
   
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
-sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu noble stable"
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu jammy stable"
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
 
 
 EOF
